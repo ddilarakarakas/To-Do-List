dd99b015c862c591758bdb8bed1226987e12a369
ddilarakarakas <karakasdilara1@gmail.com>
2023-01-07 23:38:46 +0300

    Opening Task Details

--- ToDoList/TaskViewController.swift
+++ ToDoList/TaskViewController.swift
@@ -9,21 +9,24 @@ import UIKit
 
 class TaskViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    var task: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        label.text = task
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Delete", style: .done, target: self, action: #selector(deleteTask))
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func deleteTask(){
+//        guard let count = UserDefaults().value(forKey: "count") as? Int else {
+//            return
+//        }
+//        let newCount = count - 1
+//
+//        UserDefaults().set(newCount, forKey: "count")
+//        UserDefaults().set(nil, forKey: "task_ \(current)")
     }
-    */
 
 }
