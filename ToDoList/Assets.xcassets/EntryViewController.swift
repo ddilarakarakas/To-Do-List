be260bbb71a5cb6e135e6bef94cb6f3f2759c0ae
ddilarakarakas <karakasdilara1@gmail.com>
2023-01-07 21:48:04 +0300

    TableView Outlet

---
+++ ToDoList/Assets.xcassets/EntryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  EntryViewController.swift
+//  ToDoList
+//
+//  Created by Dilara Karakaş on 7.01.2023.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController {
+
+    @
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
